a846996c9641f281aac75f09b98011d351644d0e
Tim Schmidt <schmidt.tim.j@gmail.com>
2017-03-29 18:51:41 -0400

    style.cc adjustments

--- app/scripts/services/Room.js
+++ app/scripts/services/Room.js
@@ -2,6 +2,10 @@
     function Room($firebaseArray) {
         var ref = firebase.database().ref().child("rooms");
         var rooms = $firebaseArray(ref);
+        rooms.$add({ foo: "bar" }).then(function(ref) {  //Method added from checkpoint 3. needs review
+            var id = ref.key;
+            rooms.$indexFor(id);
+        });
         
         return {
             all: rooms
