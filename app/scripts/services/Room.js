494ace7eb5ec9b3feb68efcd5bb1e8ac0cb1dfa9
Tim Schmidt <schmidt.tim.j@gmail.com>
2017-03-29 23:24:28 -0400

    wip seting up ui bootstrap and modal

--- app/scripts/services/Room.js
+++ app/scripts/services/Room.js
@@ -2,13 +2,15 @@
     function Room($firebaseArray) {
         var ref = firebase.database().ref().child("rooms");
         var rooms = $firebaseArray(ref);
-        rooms.$add({ foo: "bar" }).then(function(ref) {  //Method added from checkpoint 3. needs review
+        rooms.$add({ room: newRoom }).then(function(ref) {  //Method added from checkpoint 3. needs review
             var id = ref.key;
+            console.log("added record with id " + id);
             rooms.$indexFor(id);
         });
         
         return {
             all: rooms
+            
         };
     }
     
