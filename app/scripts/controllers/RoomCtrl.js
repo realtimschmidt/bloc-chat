d46481f38d51201fd97bacd53af1367cac7e32b6
Tim Schmidt <schmidt.tim.j@gmail.com>
2017-04-06 17:35:44 -0400

    wip messages and roomId connected to firebase objects

--- app/scripts/controllers/RoomCtrl.js
+++ app/scripts/controllers/RoomCtrl.js
@@ -1,9 +1,11 @@
 (function() {
-    //function RoomCtrl(Message, Room, $uibModal) {
-    function RoomCtrl(Room, $uibModal) {
+    function RoomCtrl(Message, Room, $uibModal) {
+    //function RoomCtrl(Room, $uibModal) {
         this.rooms = Room.all;
-        this.messagesTitle = 'Default Room Title';
         this.messages = 'Default Messages';
+        this.currentRoom = null;
+
+        
         
         this.openModal = function () {
             var modalInstance = $uibModal.open({
@@ -12,16 +14,15 @@
             });
         };
         
-        this.selectRoom = function() {
-            //this.messagesTitle = getByRoomId();
-            this.messagesTitle = "Something to get room ID";
-            this.messages = "Something to get messages";
+        this.selectRoom = function(room) {
+            this.currentRoom = room;
+            this.messages = Message.getByRoomId(room.$id);
         }
         
     }
     
     angular
         .module('blocChat')
-        //.controller('RoomCtrl', ['Message', 'Room', '$uibModal', RoomCtrl]);
-        .controller('RoomCtrl', ['Room', '$uibModal', RoomCtrl]);
+        .controller('RoomCtrl', ['Message', 'Room', '$uibModal', RoomCtrl]);
+        //.controller('RoomCtrl', ['Room', '$uibModal', RoomCtrl]);
 })();
